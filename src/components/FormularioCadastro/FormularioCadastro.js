7b61e86f55fc64ab322f52f3d7b8f2d988849c01
mbiga <mbigaram.mb@gmail.com>
2022-10-19 11:53:49 -0300

    Execicios Fluxo de Dados

--- src/components/FormularioCadastro/FormularioCadastro.js
+++ src/components/FormularioCadastro/FormularioCadastro.js
@@ -1,6 +1,25 @@
 import { FormContainer, Form, Input, StyledLabel } from "./styled";
 
-export const FormularioCadastro = () => {
+
+
+
+export const FormularioCadastro = (props) => {
+
+ 
+
+  function onChangeUrl(e) {
+    props.setUrlFoto(e.target.value)
+  }
+
+  function onChangeDescricao(event){
+    props.setDescricao(event.target.value)
+  }
+
+  function onChangeTitulo(event){
+    props.setTitulo(event.target.value)
+  }
+
+
 
   return (
     <FormContainer>
@@ -8,11 +27,15 @@ export const FormularioCadastro = () => {
       <Form>
         <StyledLabel htmlFor="foto">
           Imagem:
-          <Input id="foto" />
+          <Input id="foto" value={props.urlFoto} onChange={onChangeUrl} />
         </StyledLabel>
         <StyledLabel htmlFor="descricao">
           Descrição:
-          <Input id="descricao" />
+          <Input id="descricao" value={props.descricao} onChange={onChangeDescricao}/>
+        </StyledLabel>
+        <StyledLabel htmlFor="titulo">
+          Titulo do post:
+          <Input id="descricao" value={props.titulo} onChange={onChangeTitulo}/>
         </StyledLabel>
       </Form>
     </FormContainer>
